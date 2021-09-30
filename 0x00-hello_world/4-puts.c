4ddddc09f12bd0b9fb7aab7b0e94348efd2f1924
Astambar <leuchartjason@gmail.com>
2021-09-30 10:24:29 +0000

    adding 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
-int main(int argc, char const *argv[])
+/*
+	 * This is the preferred style for multi-line
+	 * comments in C source code.
+	 * Please use it consistently.
+	 *
+	 * Description:  A column of asterisks on the left side,
+	 * with beginning and ending almost-blank lines.
+     *
+	 */
+int main(void)
 {
-    /* code */
-    puts("\"Programming is like building a multilingual puzzle");
-    return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
