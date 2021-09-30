2bd55d16079a5baac570ac71019df35a020155cc
Astambar <leuchartjason@gmail.com>
2021-09-30 13:29:34 +0000

    adding

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
 #include <string.h>
 #include <unistd.h>
 /**
- * main - prints exactly 
+ * main - prints exactly
  * "and that piece of art is useful"
  * - Dora Korpar, 2015-10-19",
  * followed by a new line, to the standard error.
