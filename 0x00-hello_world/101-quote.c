f92a1df9ee18066462f93ec04f5bf485c2c1754e
Astambar <leuchartjason@gmail.com>
2021-09-30 13:27:55 +0000

    adding

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,7 +3,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-
 /**
  * main - prints exactly "and that piece of art is useful"
  * - Dora Korpar, 2015-10-19",
@@ -11,7 +10,7 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{	
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
-}
+	{
+		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+		return (1);
+	}
