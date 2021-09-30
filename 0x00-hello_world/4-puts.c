c6846399e6cda43205e2172cc51d2987dfd6b96d
Astambar <leuchartjason@gmail.com>
2021-09-30 10:27:08 +0000

    adding 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 /*
-*main puts text 
-*/
+	 * This is the preferred style for multi-line
+	 * comments in C source code.
+	 * Please use it consistently.
+	 *
+	 * Description:  A column of asterisks on the left side,
+	 * with beginning and ending almost-blank lines.
+     * Main inutile fonction
+	 */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
