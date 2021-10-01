63a70c928be4392d3e0d59d2153bf22ac4b242e5
Astambar <leuchartjason@gmail.com>
2021-10-01 13:55:38 +0000

    adding 0,1,2 and 3 file

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,12 +9,12 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-	int n;
+    int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-    
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
+    /* your code goes there */
+
     if(n % 10 > 5)
     {
         printf("Last digit of %d is %d and is greater than 5\n", n , n % 10);
@@ -28,5 +28,5 @@ int main(void)
         printf("Last digit of %d is %d and is 0\n", n , n % 10);
     }
 
-	return (0);
+    return (0);
 }
