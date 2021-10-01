eb23537995b137726e90dc9aa5d207c30addbb9f
Astambar <leuchartjason@gmail.com>
2021-10-01 13:08:07 +0000

    adding 0,1,2 and 3 file

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,11 +21,11 @@ int main(void)
     }
     else if(n == 0)
     {
-        printf("Last digit of %d and is 0 \n", n);
+        printf("Last digit of %d and is %d \n", n , n % 10);
     }
     else if(n < 6)
     {
-        printf("Last digit of %d and is less than 6 and not 0 \n", n);
+        printf("Last digit of %d and is %d less than 6 and not 0 \n", n , n % 10);
     
     }
 
