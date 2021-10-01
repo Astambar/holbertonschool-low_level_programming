a6ad43cf76370a755b93b26b75a05a61a935cb91
Astambar <leuchartjason@gmail.com>
2021-10-01 12:48:37 +0000

    adding 0,1,2 and 3 file

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,15 +17,15 @@ int main(void)
 	/* your code goes there */
 	if (n == 0)
 	{
-		printf("is zero");
+		printf("%d is zero",n);
 	}
 	else if (n < 0)
 	{
-		printf("is negative");
+		printf("%d is negative",n);
 	}
 	else
 	{
-		printf("is positive");
+		printf("%d is positive",n);
 	}
 	return (0);
 }
