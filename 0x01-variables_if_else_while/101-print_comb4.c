a455cda4ea89f10db3fb4fd9efa32321c4bb44f7
Astambar <leuchartjason@gmail.com>
2021-10-01 23:49:28 +0000

    adding 0,1,2 and 3 file

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+/**
+ * main - condition positive or negative or 0
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int c = 48;
+	int f = 49;
+	int g = 50;
+
+	while (c < 57)
+	{
+		while (f < 58)
+		{
+		if (f > c)
+		{
+			while (g < 58)
+			{
+				if (c != g || c != f || f != g)
+				{
+					if (c != 56 || f != 57 || g != 55)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+
+				}
+
+				g++;
+			}
+
+		}
+		f++;
+		}
+		f = 48;
+		c++;
+
+	}
+	putchar('\n');
+	return (0);
+
+}
