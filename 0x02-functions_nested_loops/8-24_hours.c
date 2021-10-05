a0ceb49ab3008b98a2e1282e2601c4ca0f06c0f5
Astambar <leuchartjason@gmail.com>
2021-10-05 15:34:24 +0000

    0

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,5 +7,23 @@
 
 void jack_bauer(void)
 {
+	int h, m;
 
+	h = 0;
+
+	while (h < 24)
+	{
+		m = 0;
+		while (m < 60)
+		{
+			_putchar((h / 10) + 48);
+			_putchar((h % 10) + 48);
+			_putchar(':');
+			_putchar((m / 10) + 48);
+			_putchar((m % 10) + 48);
+			_putchar('\n');
+			m++;
+		}
+		h++;
+	}
 }
