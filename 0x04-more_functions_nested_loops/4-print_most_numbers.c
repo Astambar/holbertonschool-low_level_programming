a311ac68cdf2083941c57af6baeb47900d830fd6
Astambar <leuchartjason@gmail.com>
2021-10-07 20:18:37 +0000

    0

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,12 +10,12 @@ void print_most_numbers(void)
 {
 	int i;
 
-	i = 0;
+	i = -1;
 	while (i++ < 10)
 	{
 		if (i != 2 && i != 4)
 		{
-		_putchar(i + 47;
+		_putchar(i + '0';
 		}
 	}
 	_putchar('\n');
