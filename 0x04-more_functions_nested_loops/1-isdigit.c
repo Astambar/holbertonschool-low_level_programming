b4e4a24ca6a5c97b4171f7d343a77ad27eee8561
Astambar <leuchartjason@gmail.com>
2021-10-07 08:58:27 +0000

    0

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - check the code.
+ * @c: - verif uppercase
+ * Return: Always 0.
+ */
+
+int _isdigit(int c)
+{
+	if (0 <= c || c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
