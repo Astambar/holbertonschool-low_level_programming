0513636258afa8a89696246f5135eb91a10ff14b
Astambar <leuchartjason@gmail.com>
2021-10-07 15:01:41 +0000

    0

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _isupper - check the code.
+ * @c: - verif uppercase
+ * Return: Always 0.
+ */
+
+void print_square(int size)
+{
+	int hauteur, largeur;
+	if (size <= 0)
+	{
+		_putchar('\n');
+	} 
+	else
+	{
+		for (hauteur = 0 ; hauteur < size; hauteur++)
+		{
+			for (largeur = 0; largeur < size; largeur++)
+			{
+				_putchar('#');
+			}
+		_putchar('\n');
+		}
+	}
+}
