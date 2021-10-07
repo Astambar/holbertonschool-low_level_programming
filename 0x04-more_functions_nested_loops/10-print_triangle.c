6c61853ffb83695864f7d43d3e82359252f86f24
Astambar <leuchartjason@gmail.com>
2021-10-07 15:53:43 +0000

    0

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _isupper - check the code.
+ * @c: - verif uppercase
+ * Return: Always 0.
+ */
+
+void print_triangle(int size)
+{
+	int largeur, espace, i, j;
+	if (size <= 0)
+	{
+		_putchar('\n');
+	} 
+	else
+	{
+		for (largeur = 0 ; largeur < size; largeur++)
+		{	
+			espace = size - largeur;
+
+				for (i = 0 ; i < espace-1; i++)
+				{
+					_putchar(' ');
+				}
+			
+			for (j = 0 ; j <= largeur; j++)
+			{
+				_putchar('#');
+			}
+			
+		_putchar('\n');
+		}
+	}
+}
