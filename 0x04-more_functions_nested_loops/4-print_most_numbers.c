94765a64b005dfb00b69ca0b616d43abc8d98325
Astambar <leuchartjason@gmail.com>
2021-10-07 11:12:24 +0000

    0

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - check the code.
+ * @c: - verif uppercase
+ * Return: Always 0.
+ */
+
+void print_numbers(void)
+{
+int i;
+
+i = 0;
+while (i++ < 10)
+{	if(i != 2 && i != 4)
+	{
+		_putchar(i+47);
+	}
+}
+_putchar('\n');
+}
