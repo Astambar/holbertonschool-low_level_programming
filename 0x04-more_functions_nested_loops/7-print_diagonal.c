c51648118be9bdc788bc6f9c10ea8bcde3f33779
Astambar <leuchartjason@gmail.com>
2021-10-07 14:26:16 +0000

    0

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - check the code.
+ * @c: - verif uppercase
+ * Return: Always 0.
+ */
+
+void print_diagonal(int n)
+{
+int i = 0;
+
+while (i++ < n)
+{
+    _putchar('\\');
+}
+_putchar('\n');
+}
