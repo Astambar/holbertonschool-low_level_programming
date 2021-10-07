7e0c608447bdbdb15e965d9fcfa5857ad47c1291
Astambar <leuchartjason@gmail.com>
2021-10-07 08:53:19 +0000

    0

---
+++ 0x04-more_functions_nested_loops/generatorfile.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+int main ()
+{
+	int numfiles = 15;
+FILE *files[numfiles];
+for (int i = 0; i <= numfiles; i++)
+{
+    char filename[30];
+    sprintf(filename, "%d-main.c", i);
+	if(!(files[i] = fopen(filename, "r"))){
+		files[i] = fopen(filename, "w");
+	}
+}
+
+return 0;
+}
