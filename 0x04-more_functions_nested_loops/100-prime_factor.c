6be132758120cf446221ec1e0c4629bc141e841e
Astambar <leuchartjason@gmail.com>
2021-10-07 16:46:22 +0000

    0

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	long int number=612852475143,i, facteur;
+	for(i = 2; i <= number; i++)
+	{	while ((number % i) == 0)
+		{
+			number /=i;
+			facteur =i;
+		}
+			
+	}
+	printf("%ld\n",facteur);
+    return (0);
+}
