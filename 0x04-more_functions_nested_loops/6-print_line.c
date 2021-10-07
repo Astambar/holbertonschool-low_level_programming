c682a76140be9e22b26f0674f5746c6f15b7de41
Astambar <leuchartjason@gmail.com>
2021-10-07 13:58:57 +0000

    0

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - check the code.
+ * @c: - verif uppercase
+ * Return: Always 0.
+ */
+
+void print_line(int n)
+{
+int i = 0;
+
+while (i++ < n)
+{
+    _putchar('_');
+}
+_putchar('\n');
+}
