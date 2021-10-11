76ef1ef5731be73ba5e2f18ed235f7b8d7901df2
Astambar <leuchartjason@gmail.com>
2021-10-11 11:24:25 +0000

    0

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h> 
+#include <stdio.h>
+/**
+* _strlen - reset value pointer
+* @s: pointer
+*/
+void puts_half(char *str)
+{
+	int i, len;  
+    len = strlen(str);
+
+	for (i = 0; i < len/2; i++)  
+    {  
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
