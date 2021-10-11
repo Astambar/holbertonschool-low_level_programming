de9c914bebd19136651714e9dd8116c66b2c2c17
Astambar <leuchartjason@gmail.com>
2021-10-11 11:31:28 +0000

    0

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,9 +9,11 @@ void puts_half(char *str)
 {
 	int i, len;  
     len = strlen(str);
-	for (i = len; i > len/2; i--)  
-    {  
-			_putchar(str[i]);
+	for (i = 0; i < len/2; i++);
+	for (i = i; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
 	}
+	
 	_putchar('\n');
 }
