0aacf2aaccefc5973fd4a72c2f8e8dd3d3069a76
Astambar <leuchartjason@gmail.com>
2021-10-11 08:46:43 +0000

    0

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _strlen - reset value pointer
+* @s: pointer
+*/
+void print_rev(char *s)
+{
+	int i;
+	for (i = 0 ; str[i]!='\0'; i++);
+
+	for (i = 0 ; i < 0; i--)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+	
+}
