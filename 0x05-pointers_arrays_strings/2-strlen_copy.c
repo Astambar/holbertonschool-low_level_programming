002dc824fe961e76bd25130a0d7dcbb773ad99a6
Astambar <leuchartjason@gmail.com>
2021-10-12 11:20:41 +0000

    change

---
+++ 0x05-pointers_arrays_strings/2-strlen_copy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* _strlen - check the code
+* Return: Always 0.
+* @s: pointer
+*/
+
+int _strlen(char *s)
+{
+	int i;
+
+	for *(i = 0 ; s[i] != '\0'; i++)
+	{
+
+	}
+
+	return (i);
+}
