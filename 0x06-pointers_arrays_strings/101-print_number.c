ce3ad980341891e35f4802d1d85bd950f8baea61
Astambar <leuchartjason@gmail.com>
2021-10-14 19:48:44 +0000

    adding print_number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,12 +1,30 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+* print_number - reset value pointer
+*
+* Return: Always 0.
+* @n: pointer
+*/
 
 void print_number(int n)
 {
 	unsigned int number_traitement;
-	0 > n ? number_traitement = -n; _putchar('-'); : number_traitement = n;
 
+	if (n < 0)
+	{
+		number_traitement = -n;
+		_putchar('-');
+	} else
+	{
+		number_traitement = n;
+	}
 
 	if (number_traitement / 10)
-		print_number(n1 / 10);
+	{
+		print_number(number_traitement / 10);
+	}
 
 	_putchar((number_traitement % 10) + '0');
 }
