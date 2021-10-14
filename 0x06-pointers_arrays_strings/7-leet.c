d0836240cc43ab99681cb9b38b4697b1c9701510
Astambar <leuchartjason@gmail.com>
2021-10-14 19:05:03 +0000

    amélioration de leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -16,11 +16,12 @@ char *number = "4433007711";
 
 for (i = 0; s[i] != '\0'; i++)
 {
-	for (j = 0; letter[j] != '\0'; j++)
+	for (j = 0; letter[j] != ; j++)
 	{
 		if (s[i] == letter[j])
 		{
 			s[i] = number[j];
+			break;
 		}
 	}
 }
