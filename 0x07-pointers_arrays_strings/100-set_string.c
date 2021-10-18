feffa1bd9c1a622831da2f3abc35b32a139d5dc3
Astambar <leuchartjason@gmail.com>
2021-10-18 19:59:42 +0000

    ajout de l'exercice 100 (advanced)

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+* set_string - reset value pointer
+*
+* Return: Always 0.
+* @s: pointer tableau
+* @to: taille du tableau
+*/
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
