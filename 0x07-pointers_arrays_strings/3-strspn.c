dee9f8acd18fd7ea152818577b9a2775ee5b0097
Astambar <leuchartjason@gmail.com>
2021-10-18 13:21:16 +0000

    Ajout exercice 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+* *_strcat - reset value pointer
+*
+* Return: Always 0.
+* @dest: pointer
+* @src: pointer
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j, match, temp = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+		if(s[i] == accept[j])
+		{
+			match = 1;
+			break;
+		}
+		else
+		{
+			match = 0;
+		}
+		}
+		if (match == 0)
+		{
+			if((i - temp) >= temp)
+			{
+				temp = i - temp;
+			}
+		}
+	}
+	return (temp - 1);
+}
