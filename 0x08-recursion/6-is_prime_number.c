f0acbc503383efcf17e4944b3694a5d11e347879
Astambar <leuchartjason@gmail.com>
2021-10-20 19:59:34 +0000

    adding exo 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+int actual_prime(int n, int i);
 
 /**
  * is_prime_number - nombre premier
@@ -9,11 +9,7 @@
  */
 int is_prime_number(int n)
 {
-	if(n <= 1)
-	{
-		return (0); 
-	}
-	return (actual_prime(n, n - 1));
+	return ((n <= 1) ? 0 : actual_prime(n, n - 1))?
 }
 
 /**
@@ -30,9 +26,6 @@ int actual_prime(int n, int i)
 		return (1);
 	}
 
-	if((n % i == 0 && i > 0)
-	{
-		return (0);
-	}
-	return actual_prime(n, i - 1));
+	return (n % i == 0 && i > 0) ? 0 : actual_prime(n, i - 1);
+
 }
