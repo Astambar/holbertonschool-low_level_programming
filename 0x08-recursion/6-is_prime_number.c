a43e995589c5e0908b842cb54f8ee6e933403bbe
Astambar <leuchartjason@gmail.com>
2021-10-20 19:48:47 +0000

    adding exo 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+
+/**
+ * is_prime_number - nombre premier
+ * @n: nombre tester
+ *
+ * Return: 1 if n is a prime number, 0 if not
+ */
+int is_prime_number(int n)
+{
+	return (n <= 1) ? 0 : actual_prime(n, n - 1);
+}
+
+/**
+ * actual_prime - 
+ * @n: nombre tester
+ * @i: iterator
+ *
+ * Return: 1 if n is prime, 0 if not
+ */
+int actual_prime(int n, int i)
+{
+	if (i == 1)
+		return (1);
+	return ((n % i == 0 && i > 0) ? 0 : actual_prime(n, i - 1));
+}
