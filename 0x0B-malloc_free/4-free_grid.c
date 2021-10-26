36effb0d5cae3f49814d8c640f42cab6e91c6dbe
Astambar <leuchartjason@gmail.com>
2021-10-26 16:02:37 +0000

    4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -14,13 +14,12 @@ void free_grid(int **grid, int height)
 {
 	int i;
 
-	if (grid == NULL || height == 0)
+	if (grid == NULL || height == NULL)
 		return;
 
 	for (i = 0; i < height; i++)
 	{
 		free(grid[i]);
 	}
-
 	free(grid);
 }
