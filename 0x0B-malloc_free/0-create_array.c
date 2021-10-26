e0fb0e246f3abd1c10c44d855dbab932a7ae028f
Astambar <leuchartjason@gmail.com>
2021-10-26 08:33:55 +0000

    add exo 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ * Return: Nothing.
+ */
+char *create_array(unsigned int size, char c)
+ {
+	 char *pointer;
+	unsigned int i;
+
+	 if(size == 0)
+	 {
+		 return (NULL);
+	 }
+
+	pointer = (char *) malloc(sizeof(char) * size);
+
+	if (pointer == NULL)
+	{
+		return (0);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		*(pointer + i) = c;
+	}
+
+	*(pointer + i) = '\0';
+
+	return (pointer);
+ }
