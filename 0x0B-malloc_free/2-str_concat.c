e1ee43c9ea50db3411b8cb6ebe33295a7ec22785
Astambar <leuchartjason@gmail.com>
2021-10-26 14:15:29 +0000

    update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,14 +3,15 @@
 #include <stdlib.h>
 
 /**
- * *_strdup - prints buffer in hexa
- * @str: char[]
+ * *str_concat - prints buffer in hexa
+ * @s1: string first
+ * @s2: string second
  * Return: Nothing.
  */
 char *str_concat(char *s1, char *s2)
 {
 	char *concat;
-	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+	unsigned int i, j = 0, len1 = 0, len2 = 0;
 
 	while (s1 && s1[len1])
 		len1++;
@@ -23,7 +24,7 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1)
 	{
-		for (i = 0 < len1; i++)
+		for (i = 0; i < len1; i++)
 		{
 			concat[i] = s1[i];
 		}
@@ -31,7 +32,7 @@ char *str_concat(char *s1, char *s2)
 
 	if (s2)
 	{
-		for (i = 0; i < (len1 + len2); i++)
+		for (; i < (len1 + len2); i++)
 		{
 			concat[i] = s2[j];
 			j++;
