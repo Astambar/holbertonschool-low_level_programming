fc99b16ede33e517bef2ab071bb3dbd17382ebba
Astambar <leuchartjason@gmail.com>
2021-10-27 20:14:12 +0000

    101-strtow.c  add

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,77 @@
+
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *argstostr- prints a grid of integers
+ * @ac: the address of the two dimensional grid
+ * @av: width of the grid
+ * Return: Nothing.
+ */
+
+char **strtow(char *str)
+{
+	int i = 0, word = 0, j = 0, len_str = 0, len_word;
+	char **word_table;
+	if(str == NULL || *str == '\0')
+	{
+		return (NULL);
+	}
+/**
+ * count words
+ */
+	if (*(str + 0) == ' ')
+	{
+		for (i = i; *(str + i) == ' '; i++)
+		{
+		}
+	}
+	for(i = i; *(str + i) != '\0'; i++)
+	{
+		if(*(str + i) == ' ')
+		{
+			for(i = i; *(str + i) == ' '; i++)
+			{
+			}
+		word++;
+		}
+	}
+	/**
+	 * TABLE
+	 */
+
+	word_table = (char **) malloc(sizeof(char *) * (word + 1));
+	if (word_table == NULL)
+	{
+		free(word_table);
+		return (NULL);
+	}
+	for(i = 0; i < word; i++)
+	{
+		j = 0;
+		if(*str == ' ')
+		{
+			while(*str == ' ')
+			{
+				str++;
+				len_str++;
+			}
+		}
+		len_word = 0;
+	while(str[len_str] != ' ' && str[len_str] != '\0')
+	{
+		len_word++;
+		len_str++;
+	}
+	word_table[i] = (char *) malloc(sizeof(char) * (len_word + 1));
+		while(*str != ' ' && *str != '\0')
+		{
+			word_table[i][j] = *str;
+			j++;
+			str++;
+		}
+		word_table[i][j] = '\0';
+	}
+	return (word_table);
+}
