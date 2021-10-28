141f3eaa75e6dfcf9eb36ddaa0252ed5b9888987
Astambar <leuchartjason@gmail.com>
2021-10-28 12:00:19 +0000

    ajout 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,23 +13,26 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *string;
-	unsigned int i = 0, j = 0, len1 = 0, len2 = 0, len_string = 0;
-
-	while (s1 && s1[len1])
-		len1++;
-	while (s2 && s2[len2])
-		len2++;
-	len_string += (n < len2) ? n : len1 + len2;
+	unsigned int i = 0, j = 0, len_s1 = 0, len_s2 = 0, len_string = 0;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s1 && s1[len_s1])
+		len_s1++;
+	while (s2 && s2[len_s2])
+		len_s2++;
+	len_string += (n < len_s2) ? n : len_s1 + len_s2;
 	string = malloc(sizeof(char) * (len_string) + 5);
 
 	if (!string)
 		return (NULL);
 
-	while (i < len1)
+	while (i < len_s1)
 		string[i] = s1[i], i++;
-	len_string = (n < len2) ? (len1 + n) : (len1 + len2);
-		while (i < (len_string))
-			string[i++] = s2[j++];
+	len_string = (n < len_s2) ? (len_s1 + n) : (len_s1 + len_s2);
+	while (i < (len_string))
+		string[i++] = s2[j++];
 	string[i] = '\0';
 	return (string);
 }
