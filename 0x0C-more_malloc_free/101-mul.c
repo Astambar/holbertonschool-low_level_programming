bdb6f9f8aa7f78d2b2a86bbd60644a081d3289b8
Astambar <leuchartjason@gmail.com>
2021-10-28 20:59:11 +0000

    test

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,38 +3,43 @@
 #include "main.h"
 
 /**
- * is_digit - verifie que la chaine est un nombre
- * @s: chaine à tester
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
  *
- * Return: 0 si c'est pas un digit sinon 1
+ * Return: 0 if a non-digit is found, 1 otherwise
  */
 int is_digit(char *s)
 {
 	int i = 0;
 
-	for (; s[i]; i++)
+	while (s[i])
+	{
 		if (s[i] < '0' || s[i] > '9')
 			return (0);
+		i++;
+	}
 	return (1);
 }
 
 /**
- * _strlen - retourne la longueur de la chaine
- * @s: chaine à tester
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
  *
- * Return: la longeur de la chaine
+ * Return: the length of the string
  */
 int _strlen(char *s)
 {
 	int i = 0;
 
-	for (; s[i] != '\0'; i++)
-	;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
 	return (i);
 }
 
 /**
- * errors - gestion d'erreur du main
+ * errors - handles errors for main
  */
 void errors(void)
 {
@@ -43,45 +48,43 @@ void errors(void)
 }
 
 /**
- * main - multiplies deux nombre positif
- * @argc: argument = nombre
- * @argv: tableau d' arguments
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
- * Return: toujour 0 en cas de succés
+ * Return: always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-	char *string1, *string2;
-	int len_digit1, len_digit2, len_digit, i, calculate, digit1, digit2, *result, a = 0;
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
-	string1 = argv[1], string2 = argv[2];
-	if (argc != 3 || !is_digit(string1) || !is_digit(string2))
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 		errors();
-
-	len_digit1 = _strlen(string1), len_digit2 = _strlen(string2);
-	len_digit = len_digit1 + len_digit2 + 1;
-	result = malloc(sizeof(int) * len_digit);
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
 	if (!result)
 		return (1);
-	for (i = 0; i <= len_digit1 + len_digit2; i++)
+	for (i = 0; i <= len1 + len2; i++)
 		result[i] = 0;
-	/* calcul result*/
-	for (len_digit1 -= 1; len_digit1 >= 0; len_digit1--)
+	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
-		digit1 = string1[len_digit1] - '0';
-		calculate = 0;
-		for (len_digit2 = _strlen(string2) - 1; len_digit2 >= 0; len_digit2--)
+		digit1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
-			digit2 = string2[len_digit2] - '0';
-			calculate += result[len_digit1 + len_digit2 + 1] + (digit1 * digit2);
-			result[len_digit1 + len_digit2 + 1] = calculate % 10;
-			calculate /= 10;
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
 		}
-		if (calculate > 0)
-			result[len_digit1 + len_digit2 + 1] += calculate;
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
 	}
-	/* print result */
-	for (i = 0; i < len_digit - 1; i++)
+	for (i = 0; i < len - 1; i++)
 	{
 		if (result[i])
 			a = 1;
