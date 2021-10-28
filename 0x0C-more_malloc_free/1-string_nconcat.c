032e048935a4ef95ea268efbdeec6a249d0db28e
Astambar <leuchartjason@gmail.com>
2021-10-28 12:21:47 +0000

    ajout 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -20,7 +20,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	while (s2 && s2[len_s2])
 		len_s2++;
 	len_string += (n < len_s2) ? n : len_s1 + len_s2;
-	string = malloc(sizeof(char) * (len_string) + 5);
+	string = malloc(sizeof(char) * (len_string) + 1);
 
 	if (!string)
 		return (NULL);
@@ -30,7 +30,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	len_string = (n < len_s2) ? (len_s1 + n) : (len_s1 + len_s2);
 	if (s2)
 		while (i < (len_string))
-			string[i + j] = s2[j], i++, j++;
-	string[i + j] = '\0';
+			string[i++] = s2[j++];
+	string[i] = '\0';
 	return (string);
 }
