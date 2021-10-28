2b08cc9426ade2a88f019547fb163f2315a843b5
Astambar <leuchartjason@gmail.com>
2021-10-28 15:07:42 +0000

    adding 2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *_memset - check the code
+ *@string: chaine 1
+ *@b: chaine 2
+ *@n: taille allocation
+ * Return: Always 0.
+ */
+char *_memset(char *string, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		string[i] = b;
+	return (string);
+}
+/**
+ * *_calloc - check the code
+ *@nmemb: mem
+ *@size: taille allocation
+ * Return: Always 0.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *var_memory_allocation;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	var_memory_allocation = malloc(size * nmemb);
+
+	if (var_memory_allocation == NULL)
+		return (NULL);
+
+	_memset(var_memory_allocation, 0, nmemb * size);
+
+	return (var_memory_allocation);
+}
