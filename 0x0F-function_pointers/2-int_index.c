cbc9a90f198cf6e41d73d38798845c07b7fb59d4
Astambar <leuchartjason@gmail.com>
2021-11-03 13:29:55 +0000

    Ajout du 2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,17 @@
+/**
+ * int_index - ma fonction de teste d'index
+ * @array: mon tableau
+ * @size:	la taille du tableau
+ * @cmp: ma fonction
+ * Return: index
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+	if (array && cmp)
+		for (i = 0; i < size; i++)
+			if((*cmp)(array[i]) != 0)
+				return(i);
+	return(-1);
+}
