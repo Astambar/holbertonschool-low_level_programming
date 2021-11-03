a92e3d1311e44611775ae6b9caf1ecfb381905e7
Astambar <leuchartjason@gmail.com>
2021-11-03 22:12:31 +0000

    update exercice 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,7 +11,7 @@
 int main(int argc, char *argv[])
 {
 	int arg1, arg2, result;
-	char o;
+	char diviseur;
 	int (*func)(int, int);
 
 	if (argc != 4)
@@ -31,9 +31,9 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
-	o = *argv[2];
+	diviseur = *argv[2];
 
-	if ((o == '/' || o == '%') && arg2 == 0)
+	if ((diviseur == '/' || diviseur == '%') && arg2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
