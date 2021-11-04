aa069cc20294420d59479b8c9c82a8f2d811c2cb
Astambar <leuchartjason@gmail.com>
2021-11-04 00:08:25 +0000

    initialisation projet variadic

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - calcul la comme de tout les paramettres
+ * @n: number of arguments passed to the function
+ *
+ * Return: the resulting sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int somme = 0;
+	va_list list_number;
+
+	va_start(list_number, n);
+
+	for (i = 0; i < n; i++)
+		somme += va_arg(list_number, int);
+
+	va_end(list_number);
+
+	return (somme);
+}
