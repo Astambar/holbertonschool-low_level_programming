f61a4be2ccd71f0324fca43d78a79c0fd0f72769
Astambar <leuchartjason@gmail.com>
2021-11-16 22:57:02 +0000

    ajout fichier manquant potentiel

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -16,8 +16,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	if (head == NULL)
 		return (NULL);
 
-	for (counter = 0; counter < index && head != NULL; counter++, head = (*head).next)
-		;
+	for (counter = 0; counter < index && head != NULL; counter++)
+	{
+		head = (*head).next;
+	}
 
 	if (counter == index)
 		return (head);
