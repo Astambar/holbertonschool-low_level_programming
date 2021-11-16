78db2e4b6e1f97d40d8c2c2ef8d8ada78e543771
Astambar <leuchartjason@gmail.com>
2021-11-16 12:16:48 +0000

    init project 0x13-more_singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,41 @@
+#ifndef LISTS_H
+#define LISTS_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+/*Prototypes*/
+listint_t *find_listint_loop(listint_t *head);
+size_t free_listint_safe(listint_t **h);
+size_t print_listint_safe(const listint_t *head);
+listint_t *reverse_listint(listint_t **head);
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
+int sum_listint(listint_t *head);
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+int pop_listint(listint_t **head);
+void free_listint2(listint_t **head);
+void free_listint(listint_t *head);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+listint_t *add_nodeint(listint_t **head, const int n);
+size_t listint_len(const listint_t *h);
+size_t print_listint(const listint_t *h);
+
+#endif /*LISTS*/
