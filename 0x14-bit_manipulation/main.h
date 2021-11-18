13221d2ec0179b4c5d526e008ad343172f966e70
Astambar <leuchartjason@gmail.com>
2021-11-18 22:15:16 +0000

    update global

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -8,6 +8,7 @@
 
 /*Prototypes*/
 int _putchar(char c);
+int power(int base, int power);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
