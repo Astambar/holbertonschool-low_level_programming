dda42eb64c7fc3f552340bf9004b05a61852b898
Astambar <leuchartjason@gmail.com>
2022-01-06 08:39:05 +0000

    first tenta

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * print_dlistint- compte le nombre d'element de la liste
+ * @h: pointer de la list
+ * Return: Le nombre d'element de la liste
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	for (;h; h = h->next, i++)
+	;
+	return (i);
+}
