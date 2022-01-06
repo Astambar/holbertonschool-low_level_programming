35712c7081e050325331a794f3a2f91cf8ffb8d4
Astambar <leuchartjason@gmail.com>
2022-01-06 13:05:45 +0000

    first tenta

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - finds a specific node of a linked list
+ * @head: pointer to the beginning of the list
+ * @index: index of the node to retrieve
+ *
+ * Return: pointer to the indexed node, or NULL on failure
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	if (!head)
+		return (NULL);
+	if (index == 0)
+		return (head);
+	for (; i < index; head = (*head).next, i++)
+		if (!(*head).next)
+			return (NULL);
+	return (head);
+}
