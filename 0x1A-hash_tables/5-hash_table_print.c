60a4881d9ae1558ad1ec92cf2f836bf33dec6e99
Astambar <leuchartjason@gmail.com>
2022-01-28 22:27:29 +0000

    push all project

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - imprime la table de hashage
+ * @ht: la table de hashage
+ * Return: Rien.
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *temp = NULL;
+	unsigned long int i = 0;
+	char *new = ", ", *space = "";
+
+	if (ht == NULL)
+		return;
+	if (ht != NULL)
+	{
+		printf("{");
+		for (; i < ht->size; i++)
+		{
+			temp = ht->array[i];
+			for (; temp; space = new, temp = new_temp->next)
+			{
+				printf("%s", space);
+				printf("'%s': '%s'", new_temp->key, new_temp->value);
+			}
+		}
+		printf("}\n");
+	}
+}
