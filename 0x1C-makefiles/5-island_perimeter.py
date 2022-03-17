ac3c5057bed3d22c678f3fd7caec1683f8b7e043
Astambar <leuchartjason@gmail.com>
2022-03-17 22:31:32 +0000

    makefile init tenta

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""returns the perimeter of the island described in grid(int list of list)"""
+
+
+def island_perimeter(grid):
+    """_summary_
+
+    Args:
+        grid (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
+
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+                if (grid[i][j - 1] == 1):
+                    edges += 1
+                if (grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
