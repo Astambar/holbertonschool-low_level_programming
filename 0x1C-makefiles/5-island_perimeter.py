bf16475bba8423056391dd41ca8918e2c349b824
Astambar <leuchartjason@gmail.com>
2022-03-17 22:44:23 +0000

    makefile init tenta 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,25 +3,18 @@
 
 
 def island_perimeter(grid):
-    """_summary_
-
-    Args:
-        grid (_type_): _description_
-
-    Returns:
-        _type_: _description_
-    """
-    width = len(grid[0])
-    height = len(grid)
+    """Return the island perimeter"""
+    wid = len(grid[0])
+    hei = len(grid)
     edges = 0
     size = 0
 
-    for i in range(height):
-        for j in range(width):
+    for i in range(hei):
+        for j in range(wid):
             if grid[i][j] == 1:
                 size += 1
-                if (grid[i][j - 1] == 1):
+                if (j > 0 and grid[i][j - 1] == 1):
                     edges += 1
-                if (grid[i - 1][j] == 1):
+                if (1 > 0 and grid[i - 1][j] == 1):
                     edges += 1
     return size * 4 - edges * 2
