7504a0d3ff70afe422794e56c5e0ee67accd10a4
Astambar <leuchartjason@gmail.com>
2022-05-09 17:44:21 +0000

    0x18-dynamic_libraries updae 0

--- 0x18-dynamic_libraries/_strlen.c
+++ 0x18-dynamic_libraries/_strlen.c
@@ -7,12 +7,12 @@
  */
 int _strlen(char *s)
 {
-    int i;
-    int length = 0;
+	int i;
+	int length = 0;
 
-    for (i = 0; s[i] != '\0'; i++)
-    {
-        length++;
-    }
-    return (length);
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		length++;
+	}
+	return (length);
 }
