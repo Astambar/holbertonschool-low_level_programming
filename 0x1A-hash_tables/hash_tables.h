77b374c3fed5425311ad4bf18cff9914fb916d68
Astambar <leuchartjason@gmail.com>
2022-05-27 21:28:58 +0000

    test

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,15 +1,18 @@
-#ifndef _HASH_TABLES_H_
-#define _HASH_TABLES_H_
+#ifndef HASH_TABLES_H
+#define HASH_TABLES_H
 
+#include <stddef.h>
 #include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
+#include <string.h>
+
+/*============================================================*/
+/*structures*/
 
 /**
  * struct hash_node_s - Node of a hash table
  *
- * @key: The key, string
- * The key is unique in the HashTable
+ * @key: The key, string. The key is unique in the HashTable
  * @value: The value corresponding to a key
  * @next: A pointer to the next node of the List
  */
@@ -25,6 +28,7 @@ typedef struct hash_node_s
  *
  * @size: The size of the array
  * @array: An array of size @size
+ *
  * Each cell of this array is a pointer to the first node of a linked list,
  * because we want our HashTable to use a Chaining collision handling
  */
@@ -34,6 +38,49 @@ typedef struct hash_table_s
 	hash_node_t **array;
 } hash_table_t;
 
+/**
+ * struct shash_node_s - Node of a sorted hash table
+ *
+ * @key: The key, string. The key is unique in the HashTable
+ * @value: The value corresponding to a key
+ * @next: A pointer to the next node of the List
+ * @sprev: A pointer to the previous element of the sorted linked list
+ * @snext: A pointer to the next element of the sorted linked list
+ */
+typedef struct shash_node_s
+{
+	char *key;
+	char *value;
+	struct shash_node_s *next;
+	struct shash_node_s *sprev;
+	struct shash_node_s *snext;
+} shash_node_t;
+
+/**
+ * struct shash_table_s - Sorted hash table data structure
+ *
+ * @size: The size of the array
+ * @array: An array of size @size
+ * Each cell of this array is a pointer to the first node of a linked list,
+ * because we want our HashTable to use a Chaining collision handling
+ * @shead: A pointer to the first element of the sorted linked list
+ * @stail: A pointer to the last element of the sorted linked list
+ */
+typedef struct shash_table_s
+{
+	unsigned long int size;
+	shash_node_t **array;
+	shash_node_t *shead;
+	shash_node_t *stail;
+} shash_table_t;
+
+/*============================================================*/
+/*functions created before*/
+hash_node_t *add_node(hash_node_t *array, const char *key, const char *value);
+void free_list(hash_node_t *head);
+
+/*============================================================*/
+/*prototypes for the exercices*/
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
@@ -42,12 +89,21 @@ char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
 
-shash_table_t *shash_table_create(unsigned long int size)
-void shash_table_print_rev(const shash_table_t *ht)
-void shash_table_print(const shash_table_t *ht)
-void node_sorted_by_hash_key(shash_table_t *ht, shash_node_t *node)
-int shash_table_set(shash_table_t *ht, const char *key, const char *value)
-shash_node_t *create_node(const char *key, const char *value)
-void shash_table_delete(shash_table_t *ht)
-char *shash_table_get(const shash_table_t *ht, const char *key)
-#endif /* _HASH_TABLES_H_ */
+/*advanced*/
+shash_table_t *shash_table_create(unsigned long int size);
+int shash_table_set(shash_table_t *ht, const char *key, const char *value);
+char *shash_table_get(const shash_table_t *ht, const char *key);
+void shash_table_print(const shash_table_t *ht);
+void shash_table_print_rev(const shash_table_t *ht);
+void shash_table_delete(shash_table_t *ht);
+shash_node_t *sadd_node(shash_node_t *array, const char *key, const char *value);
+void sfree_list(shash_node_t *head);
+shash_node_t *sort_shash_list(shash_table_t *ht, unsigned long int index, const char *key);
+unsigned long int shash_djb2(const unsigned char *str);
+unsigned long int skey_index(const unsigned char *key, unsigned long int size);
+
+/*============================================================*/
+/*function creates in exercices*/
+
+
+#endif /* HASH_TABLES_H */
