345e36ca050684c8ac231d21ffded4157549f52f
Astambar <leuchartjason@gmail.com>
2022-08-31 20:32:24 +0000

    project

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -15,10 +15,12 @@
 
 int linear_search(int *array, size_t size, int value)
 {
+	size_t i = 0;
+
 	if (size == 0)
 		return (-1);
 
-	for (size_t i = 0; array[i] && i < size; i++)
+	for (; array[i] && i < size; i++)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
